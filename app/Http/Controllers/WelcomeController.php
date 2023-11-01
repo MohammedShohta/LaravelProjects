3fc8474f1838247424d32899e12e155d3a5dfdaf
unknown <mohammedshohta1234@gmail.com>
2023-11-01 10:45:21 +0200

    Initial commit

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use ses;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+       return "welcome controller";
+
+
+
+    }
+
+
+}
